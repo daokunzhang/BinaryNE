a910a151d25cd5084eec492206699c2c28616de7
Daokun Zhang <daokunzhang2015@gmail.com>
2020-06-29 15:58:19 +0800

    upload BinaryNE code

---
+++ BinaryNERun.sh
@@ -0,0 +1,2 @@
+./BinaryNE -graph cora.txt -output cora_BinaryNE_emb.txt -time cora_BinaryNE_time.txt -size 128 -window 10 -walknum 40 -walklen 100 -samples 100
+./BinaryNE -graph citeseer.txt -output citeseer_BinaryNE_emb.txt -time citeseer_BinaryNE_time.txt -size 128 -window 10 -walknum 40 -walklen 100 -samples 100
